1f58a4945b558e93dffe6eeca51e932a34fee8eb
양기현 <rlgusdid@naver.com>
2020-10-07 16:26:11 +0900

    카테고리 캐시 업데이트 기능 구현 및 Spring boot Security 연동
    * 트래픽이 적을 것으로 예상되는 category 데이터를 직접 추가할 때마다 캐시 업데이트 하는 기능 구현
    * category 수정 시 캐시 업데이트 하는 기능 구현
    * Spring boot Security를 연동하여 모든 API의 권한을 ADMIN으로 설정

---
+++ src/main/java/com/shoppingmall/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.shoppingmall.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Slf4j
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().hasAnyAuthority("ROLE_ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
